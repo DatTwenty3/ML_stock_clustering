168e9096f6b46537a3fca5a1c389bc0671b34480
Dat Le <datle.hcmut@gmail.com>
2024-04-05 14:28:31 +0700

    - update function's position in code

--- sample.py
+++ sample.py
@@ -11,6 +11,60 @@ from sklearn.cluster import KMeans
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
 
+#Tạo các Centroid theo phương random
+k  = 3
+def select_initial_centers(X, k):
+    centers = []
+    centers.append(X[np.random.choice(X.shape[0])])
+    for _ in range(1, k):
+        distances = np.array([min([np.linalg.norm(x-c) for c in centers]) for x in X])
+        probabilities = distances / distances.sum()
+        cum_probabilities = probabilities.cumsum()
+        r = np.random.rand()
+        for j, p in enumerate(cum_probabilities):
+            if r < p:
+                i = j
+                break
+        centers.append(X[i])
+    return centers
+
+#Các hàm sử dụng để update các Centroid
+def distance(p1,p2):
+    return np.sqrt(np.sum((p1-p2)**2))
+
+def assign_clusters(X, clusters):
+    for idx in range(X.shape[0]):
+        dist = []
+
+        curr_x = X[idx]
+
+        for i in range(k):
+            dis = distance(curr_x, clusters[i]['center'])
+            dist.append(dis)
+        curr_cluster = np.argmin(dist)
+        clusters[curr_cluster]['points'].append(curr_x)
+    return clusters
+
+# Implementing the M-Step
+def update_clusters(X, clusters):
+    for i in range(k):
+        points = np.array(clusters[i]['points'])
+        if points.shape[0] > 0:
+            new_center = points.mean(axis=0)
+            clusters[i]['center'] = new_center
+
+            clusters[i]['points'] = []
+    return clusters
+
+def pred_cluster(X, clusters):
+    pred = []
+    for i in range(X.shape[0]):
+        dist = []
+        for j in range(k):
+            dist.append(distance(X[i],clusters[j]['center']))
+        pred.append(np.argmin(dist))
+    return pred
+
 # Đường dẫn đến thư mục chứa các tệp dữ liệu cổ phiếu
 folder_path = "Stock_for_clustering(S&P500)"
 
@@ -41,12 +95,10 @@ else:
     prices_df = pd.concat(prices_list, axis=1)
     prices_df.sort_index(inplace=True)
     print(prices_df)
-
     # Tính toán lợi suất hàng năm (returns) và độ biến động (volatility)
     returns = pd.DataFrame()
     returns['Returns'] = (prices_df.pct_change(fill_method=None)/100).mean() * 252
     returns['Volatility'] = (prices_df.pct_change(fill_method=None)/100).std() * sqrt(252)
-
     # Thực hiện gom cụm sử dụng dữ liệu returns
     X = returns.values
 
@@ -54,33 +106,15 @@ else:
 data = np.asarray([np.asarray(returns['Returns']),np.asarray(returns['Volatility'])]).T
 x = data
 distorsions = []
-for k in range(2, 15):
-    k_means = KMeans(n_clusters=k)
+for n in range(2, 15):
+    k_means = KMeans(n_clusters = n)
     k_means.fit(x)
     distorsions.append(k_means.inertia_)
 #fig = plt.figure(figsize=(15, 5))
-
 plt.plot(range(2, 15), distorsions)
 plt.grid(True)
 plt.title('Elbow curve')
 plt.show()
-
-#Tạo các Centroid theo phương pháp Al Daoud.
-k  = 3
-def select_initial_centers(X, k):
-    centers = []
-    centers.append(X[np.random.choice(X.shape[0])])
-    for _ in range(1, k):
-        distances = np.array([min([np.linalg.norm(x-c) for c in centers]) for x in X])
-        probabilities = distances / distances.sum()
-        cum_probabilities = probabilities.cumsum()
-        r = np.random.rand()
-        for j, p in enumerate(cum_probabilities):
-            if r < p:
-                i = j
-                break
-        centers.append(X[i])
-    return centers
 
 # Sử dụng hàm select_initial_centers để chọn các centroid ban đầu
 centers = select_initial_centers(X, k)
@@ -105,45 +139,6 @@ for i in clusters:
     center = clusters[i]['center']
     plt.scatter(center[0], center[1], marker='*', c='red')
 plt.show()
-
-#Các hàm sử dụng để update các Centroid
-def distance(p1,p2):
-    return np.sqrt(np.sum((p1-p2)**2))
-
-
-def assign_clusters(X, clusters):
-    for idx in range(X.shape[0]):
-        dist = []
-
-        curr_x = X[idx]
-
-        for i in range(k):
-            dis = distance(curr_x, clusters[i]['center'])
-            dist.append(dis)
-        curr_cluster = np.argmin(dist)
-        clusters[curr_cluster]['points'].append(curr_x)
-    return clusters
-
-
-# Implementing the M-Step
-def update_clusters(X, clusters):
-    for i in range(k):
-        points = np.array(clusters[i]['points'])
-        if points.shape[0] > 0:
-            new_center = points.mean(axis=0)
-            clusters[i]['center'] = new_center
-
-            clusters[i]['points'] = []
-    return clusters
-
-def pred_cluster(X, clusters):
-    pred = []
-    for i in range(X.shape[0]):
-        dist = []
-        for j in range(k):
-            dist.append(distance(X[i],clusters[j]['center']))
-        pred.append(np.argmin(dist))
-    return pred
 
 clusters = assign_clusters(X,clusters)
 clusters = update_clusters(X,clusters)
